511d0c33920bc83b2398d76d29682d92a230b470
Oncu <oohancan@gmail.com>
2022-04-06 06:01:35 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Oncu Ohancan on 6.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
